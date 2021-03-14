cd971f8be09862598696c0ec94e47acda11787a2
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-14 13:25:44 +0000

    part1 : added anecdotes

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button> 
+  )
+}
+
+const Display = ({text}) => {
+  return (
+    <p>{text}</p>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const handleClick = () => {
+    const r = Math.floor(Math.random() * anecdotes.length)
+    console.log(anecdotes[r])
+    setSelected(r)
+  }
+
+  return (
+    <div>
+      <Display text={anecdotes[selected]} />
+      <Button handleClick={handleClick} text="next anecdote" />
+    </div>
+  )
+}
+
+export default App
