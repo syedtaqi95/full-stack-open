29988d5d5cabd759d2813d5f04e332fa273d90c5
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-14 17:57:51 +0000

    part2 : added form to notes app

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+
+  const AddNote = (e) => {
+    e.preventDefault()
+    const noteObj = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+    }
+
+    setNotes(notes.concat(noteObj))
+    setNewNote('')
+  }
+
+  const handleNewNote = (e) => {
+    console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -10,6 +31,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={AddNote}>
+        <input value={newNote} onChange={handleNewNote} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
