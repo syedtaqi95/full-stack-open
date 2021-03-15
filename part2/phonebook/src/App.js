379d269cce1f476d8c9ef491488d2a1beb523355
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-15 19:35:22 +0000

    part2 phonebook : completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,14 +10,17 @@ const Number = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   const addNumber = (e) => {
     e.preventDefault()
-
     const newEntry = { name: newName, number: newNumber}
 
     if (persons.some(p => p.name === newName)) {
@@ -32,13 +35,27 @@ const App = () => {
 
   const handleNewName = (e) => setNewName(e.target.value)
   const handleNewNumber = (e) => setNewNumber(e.target.value)
+  const handleFilterName = (e) => setFilterName(e.target.value)
+
+  const personsToShow = (filterName === "") ? [...persons]
+  : persons.filter( (person) => {
+        const nameLowerCase = person.name.toLowerCase()
+        const filterNameLower = filterName.toLowerCase()
+        return nameLowerCase.includes(filterNameLower)
+      }
+    )  
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterName} onChange={handleFilterName} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNumber}>
         <div>
           name: <input autoFocus value={newName} onChange={handleNewName} />
+          <br />
           number: <input value={newNumber} onChange={handleNewNumber} />
         </div>
         <div>
@@ -47,7 +64,7 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(person => <Number key={person.name} person={person} />)}
+      {personsToShow.map(person => <Number key={person.name} person={person} />)}
     </div>
   )
 }
