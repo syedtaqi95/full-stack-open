ebb8a5381d742ae94c713ac5a74803128e2d8a6d
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-15 22:31:55 +0000

    part2 : created "countries"

---
+++ part2/countries/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        find countries <input />
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
