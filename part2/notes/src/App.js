cca6164963e2838a62cc34670434271668519f0d
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-18 17:27:49 +0000

    part2 notes: moved components to separate files

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,32 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import Note from './Note'
+import Note from './components/Note'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 import noteService from './services/notes'
-
-const Notification = ({ message }) => {
-  if (message === null) {
-    return null
-  }
-
-  return (
-    <div className="error">
-      {message}
-    </div>
-  )
-}
-
-const Footer = () => {
-  const footerStyle = {
-    color: 'green',
-    fontStyle: 'italic',
-    fontSize: 16
-  }
-  return (
-    <div style={footerStyle}>
-      <br />
-      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
-    </div>
-  )
-}
 
 const App = () => {
   const [notes, setNotes] = useState([])
