809b74c70b4d99c763de64ae3374f0460ba7e4cd
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-19 18:19:31 +0000

    Added REST API routes

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -28,6 +30,30 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find((note) => note.id === id)
+    if (note) {
+        response.json(note)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
 })
 
 const PORT = 3001
