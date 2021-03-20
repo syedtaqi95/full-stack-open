e491a8354f374fd7773f4b464ccb181669f1d9d6
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-20 18:17:22 +0000

    part3 phonebook: added test phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,1 +1,38 @@
-console.log("Hello world")
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const phonebook = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get("/", (request, response) => {
+  response.send("<h1>Phonebook</h1>")
+})
+
+app.get("/api/persons", (request, response) => {
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
