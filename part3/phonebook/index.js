26e432a992671b1f1d640aa7f93be41975968a1e
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-03-21 09:25:11 +0000

    part3 phonebook: completed 3.5 POST route

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,24 +5,24 @@ app.use(express.json())
 
 let phonebook = [
   {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
   },
   {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
   },
   {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
   },
   {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
   }
 ]
 
@@ -72,6 +72,30 @@ app.delete("/api/persons/:id", (request, response) => {
   }
 })
 
+
+const generateID = () => {
+  return Math.floor( Math.random() * 200 )
+}
+
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+
+  if (!(body.name || body.number)) {
+    return response.status(400).json({
+      error: 'name and/or number missing'
+    })
+  }
+
+  const entry = {
+    name: body.name,
+    number: body.number,
+    id: generateID()
+  }
+
+  phonebook = phonebook.concat(entry)
+  response.json(entry)
+})
 
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
