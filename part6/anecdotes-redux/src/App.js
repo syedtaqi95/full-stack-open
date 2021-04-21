ae9248e0a298f112ca0c97ae11387eab97a0d10d
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-04-21 20:36:17 +0100

    6.3: implemented voting for anecdotes

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -6,7 +6,10 @@ const App = () => {
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    console.log('vote', id)
+    return {
+      type: 'VOTE',
+      data: { id }
+    }
   }
 
   return (
@@ -19,7 +22,7 @@ const App = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
           </div>
         </div>
       )}
