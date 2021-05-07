51e7cc9936def9656c379b6882bd811b1852c07e
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-05-07 20:18:55 +0100

    8.5: added optional param genre to allBooks()

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => args.author ? books.filter(book => book.author === args.author) : books,
+    allBooks: (root, args) => {
+      const filteredByAuthor = args.author ? books.filter(book => book.author === args.author) : books
+      const filteredByGenre = args.genre ? filteredByAuthor.filter(book => book.genres.includes(args.genre)) : filteredByAuthor
+      return filteredByGenre
+    },
     allAuthors: () => authors,
   },
 
