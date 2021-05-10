97308ec00e3d22f0f1c0457a6a76cc45845fde15
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-05-10 18:52:17 +0100

    8.16: added user management

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,8 +1,11 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
-const { v1: uuid } = require('uuid')
+const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
 const mongoose = require('mongoose')
 const Author = require('./models/author')
 const Book = require('./models/book')
+const User = require('./models/user')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'THIS_IS_A_SECRET_KEY'
 
 const MONGODB_URI = 'mongodb+srv://fullstack:fullstack@cluster0.nthtw.mongodb.net/library?retryWrites=true&w=majority'
 
@@ -30,11 +33,23 @@ const typeDefs = gql`
     bookCount: Int!
   }
 
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  
+  type Token {
+    value: String!
+  }
+
   type Query {
-    bookCount: Int!,
-    authorCount: Int!,
+    bookCount: Int!
+    authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    allUsers: [User!]!
+    me: User
   }
 
   type Mutation {
@@ -43,12 +58,22 @@ const typeDefs = gql`
       author: String!,
       published: Int!,
       genres: [String!]!
-    ) : Book,
+    ) : Book
 
     editAuthor(
       name: String!,
       setBornTo: Int!
-    ) : Author,
+    ) : Author
+
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 
@@ -58,6 +83,10 @@ const resolvers = {
     authorCount: () => Author.collection.countDocuments(),
     allBooks: (root, args) => Book.find({}).populate('author'), //TODO add filters
     allAuthors: () => Author.find({}),
+    allUsers: () => User.find({}),
+    me: (root, args, context) => {
+      return context.currentUser
+    }
   },
 
   Author: {
@@ -68,7 +97,12 @@ const resolvers = {
   },
 
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      // Check if user is logged in
+      if (!context.currentUser) {
+        throw new AuthenticationError('not authenticated')
+      }
+
       // Add the author if they don't exist
       let author = await Author.findOne({ name: args.author })
       if (!author) {
@@ -93,7 +127,12 @@ const resolvers = {
       return book
     },
 
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+      // Check if user is logged in
+      if (!context.currentUser) {
+        throw new AuthenticationError('not authenticated')
+      }
+
       const author = await Author.findOne({ name: args.name })
       author.born = args.setBornTo
 
@@ -106,12 +145,52 @@ const resolvers = {
       }
       return author
     },
+
+    createUser: (root, args) => {
+      const user = new User({
+        username: args.username,
+        favoriteGenre: args.favoriteGenre,
+      })
+
+      try {
+        user.save()
+      } catch (e) {
+        throw new UserInputError(e.message, {
+          invalidArgs: args
+        })
+      }
+
+      return user
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password != 'secret') {
+        throw new UserInputError('wrong credentials')
+      }
+
+      const userForToken = {
+        username: args.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    }
   }
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
