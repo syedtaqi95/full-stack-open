11d75c9149f3a61736a0868613670532f3ab71c4
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-05-21 18:25:24 +0100

    9.9: added patientor-frontend, fixed ping request

--- part9/patientor-backend/build/index.js
+++ part9/patientor-backend/build/index.js
@@ -4,14 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+app.use(cors_1.default());
 app.use(express_1.default.json());
-app.get('/ping', (req, res) => {
+app.get('/api/ping', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`pinged by ${ip}`);
     res.send('pong');
 });
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,15 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
+app.get('/api/ping', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(`pinged by ${ip}`);
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
