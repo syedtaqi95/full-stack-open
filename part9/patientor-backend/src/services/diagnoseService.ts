97133dcdb4e012f8d230603d930a1858e59dd2fb
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-05-27 19:42:06 +0100

    9.19: added types for entries

--- part9/patientor-backend/src/services/diagnoseService.ts
+++ part9/patientor-backend/src/services/diagnoseService.ts
@@ -1,7 +1,7 @@
 import diagnoses from '../../data/diagnoses';
-import { Diagnose } from '../types';
+import { Diagnosis } from '../types';
 
-const getEntries = (): Diagnose[] => {
+const getEntries = (): Diagnosis[] => {
   return diagnoses;
 };
 
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,13 +1,51 @@
 // Diagnosis data schema
-export interface Diagnose {
+export interface Diagnosis {
   code: string
   name: string
   latin?: string
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export enum Gender {
   Male = 'male',
